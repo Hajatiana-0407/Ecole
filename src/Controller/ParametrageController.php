a1f93a13b51196fa94eb4ea2dd31d2476c786421
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-02-01 03:15:28 +0300

    dinamisé l'interface admin

--- src/Controller/ParametrageController.php
+++ src/Controller/ParametrageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,51 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/parametre', name: 'parmetre_')]
 class ParametrageController extends AbstractController
 {
+    private $titleMenu;
+    private $menuListes;
+    public function __construct()
+    {
+        // TItre du menu 
+        $this->titleMenu = "Parametrage";
+
+        // Liste des menu et icone 
+        $this->menuListes = [
+            "Frais de scolarité" => [
+                'href' => '' , 
+                'role' => '',
+                'icone' => '<i class="fa-solid fa-dollar-sign"></i>'
+            ],
+            'Niveau' =>  [
+                'href' => '' , 
+                'role' => '',
+                'icone' =>'<i class="fas fa-layer-group"></i>'
+            ],
+            'Matiére' =>  [
+                'href' => '' , 
+                'role' => '',
+                'icone' => '<i class="fas fa-book"></i>'
+            ],
+            'Salaire personnel' =>  [
+                'href' => '' , 
+                'role' => '',
+                'icone' => '<i class="fas fa-hand-holding"></i>'
+            ],
+            'Utilisateur' =>  [
+                'href' => '' , 
+                'role' => '',
+                'icone' => '<i class="fas fa-user"></i>'
+            ]
+        ];
+    }
+
+
     #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('parametrage/index.html.twig', [
             'controller_name' => 'ParametrageController',
+            'titleMenu' => $this->titleMenu,
+            'menuListes' => $this->menuListes
         ]);
     }
 }
