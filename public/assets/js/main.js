e11444c5f2bcbf3a92dc8515147d8bda638862ac
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-02-01 04:56:01 +0300

    interface dinamique avec jquery

---
+++ public/assets/js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    /** side bare animation */
+    $(document).on('click', '#hide-show-menu', function () {
+        const element = $('.sidebare');
+        console.log($(element).width());
+
+        if ($(element).width() > 230) {
+            $('.attribute-menu').addClass('d-none');
+            $('.sidebare').animate({
+                width: '46px'
+            }, 250)
+            $('.corps').animate({
+                paddingLeft: '53px'
+            }, 250)
+        } else {
+            $('.sidebare').animate({
+                width: '250px'
+            }, 250)
+            $('.corps').animate({
+                paddingLeft: '260px'
+            }, 250, function () {
+                $('.attribute-menu').removeClass('d-none');
+            })
+        }
+    })
+
+    /** user info animation  */
+    $(document).on('click', '.user-info-container', function (e) {
+        e.stopPropagation();
+        const element = $('.user-info-liste');
+        const position = $(element).position().top;
+
+        if (position == -200) {
+            $('.user-info-liste-li').removeClass('d-none');
+            $(element).animate({
+                top: '50px'
+            }, 250)
+        } else {
+            $('.user-info-liste-li').addClass('d-none');
+            $(element).animate({
+                top: '-200px'
+            }, 300)
+        }
+    })
+
+    $(document).click(function (event) {
+        if (!$(event.target).closest('.user-info-container').length && !$(event.target).closest('.user-info-liste-li"r').length) {
+            $('.user-info-liste-li').addClass('d-none');
+            const element = $('.user-info-liste');
+            $(element).animate({
+                top: '-200px'
+            }, 300)
+        }
+    });
+})
