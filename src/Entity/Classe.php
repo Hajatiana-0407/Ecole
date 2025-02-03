f331511f9aebc07d099e6689a6942800e3ea1e9f
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-02-03 17:32:37 +0300

    entite Classe et Niveau

---
+++ src/Entity/Classe.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClasseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClasseRepository::class)]
+class Classe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $denomination = null;
+
+    #[ORM\ManyToOne(inversedBy: 'classes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Niveau $Niveau = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDenomination(): ?string
+    {
+        return $this->denomination;
+    }
+
+    public function setDenomination(string $denomination): static
+    {
+        $this->denomination = $denomination;
+
+        return $this;
+    }
+
+    public function getNiveau(): ?Niveau
+    {
+        return $this->Niveau;
+    }
+
+    public function setNiveau(?Niveau $Niveau): static
+    {
+        $this->Niveau = $Niveau;
+
+        return $this;
+    }
+
+
+
+}
