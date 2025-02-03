eedd361287220d8c2501588cdf01fcb66d3e3e7e
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-02-03 09:08:58 +0300

    parmetrage controller

---
+++ src/Controller/Parametrage/ParametrageController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller\Parametrage;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/parametre', name: 'parmetre_')]
+class ParametrageController extends AbstractController
+{
+    private $titleMenu;
+    private $menuListes;
+    public function __construct()
+    {
+        // TItre du menu 
+        $this->titleMenu = "Parametrage";
+
+        // Liste des menu et icone 
+        $this->menuListes = [
+            'Niveau' =>  [
+                'href' => '' , 
+                'role' => '',
+                'icone' =>'<i class="fas fa-layer-group"></i>'
+            ],
+            "Frais de scolarité" => [
+                'href' => '' , 
+                'role' => '',
+                'icone' => '<i class="fa-solid fa-dollar-sign"></i>'
+            ],
+            'Matiére' =>  [
+                'href' => '' , 
+                'role' => '',
+                'icone' => '<i class="fas fa-book"></i>'
+            ],
+            'Utilisateur' =>  [
+                'href' => '' , 
+                'role' => '',
+                'icone' => '<i class="fas fa-user"></i>'
+            ] , 
+            'Salaire personnel' =>  [
+                'href' => '' , 
+                'role' => '',
+                'icone' => '<i class="fas fa-hand-holding"></i>'
+            ],
+        ];
+    }
+
+
+    #[Route('/niveau', name: 'niveau')]
+    public function niveau(): Response
+    {
+        return $this->render('parametrage/index.html.twig', [
+            'controller_name' => 'ParametrageController',
+            'titleMenu' => $this->titleMenu,
+            'menuListes' => $this->menuListes
+        ]);
+    }
+}
